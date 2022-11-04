7c5e9c92463e0bb8b58caa8d2a4ab2cbe9688c8f
Maciej Lukaszewski <mlukaszewski86@gmail.com>
2022-11-04 22:50:04 +0100

    Added blocks to js

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
-console.log("Cześć!");
-let button1 = document.querySelector(".button1");
-let button2 = document.querySelector(".button2");
-let zdjecie = document.querySelector(".zdjecie");
-let body = document.querySelector(".body");
-let themeName = document.querySelector(".themeName");
-let listItem = document.querySelector(".listItem");
+{
+    {
+        const welcome = console.log("Cześć!")
+        welcome;
+    }
 
-button1.addEventListener("click", () => {
-    body.classList.toggle("dark");
-    themeName.innerText = body.classList.contains("dark") ? "jasne" : "ciemne";
-});
+    {
+        const button1 = document.querySelector(".button1");
+        const body = document.querySelector(".body");
+        const themeName = document.querySelector(".themeName");
 
-button2.addEventListener("click", () => {
-    zdjecie.remove();
-});
+        button1.addEventListener("click", () => {
+            body.classList.toggle("dark");
+            themeName.innerText = body.classList.contains("dark") ? "jasne" : "ciemne";
+        })
+    };
+
+
+    {
+        const button2 = document.querySelector(".button2");
+        button2.addEventListener("click", () => {
+            const zdjecie = document.querySelector(".zdjecie");
+            zdjecie.remove();
+        })
+    };
+}

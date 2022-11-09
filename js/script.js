e2a99afd032ba2f05e79c0ec418b67aed076f660
Maciej Lukaszewski <mlukaszewski86@gmail.com>
2022-11-09 20:03:57 +0100

    some blocks deleted

--- js/script.js
+++ js/script.js
@@ -1,34 +1,28 @@
 {
-    {
-        const welcome = () => {
-            console.log("Cześć!")
-        }
-
-        welcome();
+    const welcome = () => {
+        console.log("Cześć!")
     }
 
-    {
-        const onChangeBackgroundClick = () => {
-            const body = document.querySelector(".body");
-            const themeName = document.querySelector(".themeName");
+    welcome();
 
-            body.classList.toggle("dark");
-            themeName.innerText = body.classList.contains("dark") ? "jasne" : "ciemne";
-        };
+    const onChangeBackgroundClick = () => {
+        const body = document.querySelector(".body");
+        const themeName = document.querySelector(".themeName");
 
-        const button1 = document.querySelector(".button1");
-
-
-        button1.addEventListener("click", onChangeBackgroundClick)
+        body.classList.toggle("dark");
+        themeName.innerText = body.classList.contains("dark") ? "jasne" : "ciemne";
     };
 
-    const removePhoto = () => {
-        const zdjecie = document.querySelector(".zdjecie");
-        zdjecie.remove();
-    }
+    const button1 = document.querySelector(".button1");
 
-    {
-        const button2 = document.querySelector(".button2");
-        button2.addEventListener("click", removePhoto);
-    };
+
+    button1.addEventListener("click", onChangeBackgroundClick)
 };
+
+const removePhoto = () => {
+    const zdjecie = document.querySelector(".zdjecie");
+    zdjecie.remove()
+};
+
+const button2 = document.querySelector(".button2");
+button2.addEventListener("click", removePhoto);
